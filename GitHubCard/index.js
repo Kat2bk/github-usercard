3709b2cc3883315ff549557220a7cc4a50d2834e
unknown <Kat2bk@gmail.com>
2020-01-16 15:55:06 -0800

    did function for component

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,50 +1,70 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
+const followersArray = [
+  "devin5",
+  "alborja07",
+  "Seth16",
+  "MaxiCB",
+  "gideonogbuagu"
+];
 
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+followersArray.forEach(function(item) {
+  axios
+    .get(`https://api.github.com/users/${item}`)
+    .then(response => {
+      displayList.appendChild(createFollower(response));
+    })
+    .catch(error => {
+      console.log("error from the axios call looping over users", error);
+    });
+});
 
-   Skip to Step 3.
-*/
+function createFollower(obj) {
+  const cardDisplay = document.createElement("div");
+  const imgUrl = document.createElement("img");
+  const cardInfo = document.createElement("div");
+  const userTitle = document.createElement("h3");
+  const gitName = document.createElement("p");
+  const gitLocation = document.createElement("p");
+  const profileUrl = document.createElement("p");
+  const gitFollowers = document.createElement("p");
+  const gitFollowing = document.createElement("p");
+  const userBio = document.createElement("p");
 
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+  cardDisplay.appendChild(imgUrl);
+  cardDisplay.appendChild(cardInfo);
+  cardInfo.appendChild(userTitle);
+  cardInfo.appendChild(gitName);
+  cardInfo.appendChild(gitLocation);
+  cardInfo.appendChild(profileUrl);
+  cardInfo.appendChild(gitFollowers);
+  cardInfo.appendChild(gitFollowing);
+  cardInfo.appendChild(userBio);
 
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
+  cardDisplay.classList.add("card");
+  imgUrl.classList.add("img");
+  userTitle.classList.add("name");
+  gitName.classList.add("username");
+  gitLocation.classList.add("p");
+  profileUrl.classList.add("p");
+  gitFollowers.classList.add("p");
+  gitFollowing.classList.add("p");
+  userBio.classList.add("p");
 
-const followersArray = [];
+  imgUrl.setAttribute("src", obj.data.avatar_url);
 
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+  userTitle.textContent = obj.data.name;
+  gitName.textContent = obj.data.login;
+  gitLocation.textContent = obj.data.location;
+  profileUrl.textContent = obj.data.url;
+  gitFollowers.textContent = "Followers: " + obj.data.followers;
+  gitFollowing.textContent = "Following: " + obj.data.following;
+  userBio.textContent = "Bio: " + obj.data.bio;
 
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
+  return cardDisplay;
+}
 
-*/
+const displayList = document.querySelector(".cards");
+
+// displayList.append(createFollower())
 
 /* List of LS Instructors Github username's: 
   tetondan
